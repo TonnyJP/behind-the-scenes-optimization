d2a9eb02199e941c10fb688526efacad3788598f
Tonny Piper <pipertonny@gmail.com>
2024-07-11 16:31:28 +0200

    done

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,13 +1,14 @@
 import { log } from '../../log.js';
+import { memo } from 'react';
 
-export default function IconButton({ children, icon, ...props }) {
+export default memo(function IconButton({ children, icon, ...props }) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
   return (
-    <button {...props} className="button">
-      <Icon className="button-icon" />
-      <span className="button-text">{children}</span>
+    <button {...props} className='button'>
+      <Icon className='button-icon' />
+      <span className='button-text'>{children}</span>
     </button>
   );
-}
+});
